9678513088e627d7ca29fe5c6665dab3cc7ea6a9
Avernor <124289614+Avernor@users.noreply.github.com>
2023-05-21 18:52:39 +0300

    Add files via upload

--- application/settings.py
+++ application/settings.py
@@ -1,9 +1,9 @@
 # MySQL Settings
 mysql_host =  '127.0.0.1' 
 mysql_port = 3306  #this must be integer (this is wrong:'3306')
-mysql_user = 'admin'   #you must replace this value with your own settings
-mysql_passwd = '' #you must replace this value with your own settings 
-mysql_schema = 'travel_agency' #the database name, replace with your ouwn value
+mysql_user = 'root'   #you must replace this value with your own settings
+mysql_passwd = 'ioanna13' #you must replace this value with your own settings 
+mysql_schema = 'agency' #the database name, replace with your ouwn value
 
 # Webserver Settings
 # IMPORTANT: The port must be available.
